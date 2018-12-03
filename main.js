d677c27fe56db3f2c7a9b5ddc73afd16bc7dde96
Linn2punkt0 <linn.johansson7@hotmail.com>
2018-12-03 11:11:00 +0100

    Point and click-count

--- main.js
+++ main.js
@@ -13,6 +13,13 @@ const images = [
 let points = 0;
 let clicks = 0;
 
+// Show current score - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+let currentScore = document.querySelector(".points");
+currentScore.innerHTML = " Points: " + points;
+
+let currentClicks = document.querySelector(".clicks");
+currentClicks.innerHTML = " Clicks: " + clicks;
+
 //Shuffle the images and place them in the card divs - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 const cards = document.querySelectorAll(".card");
 
@@ -27,7 +34,7 @@ function shuffleImages(imageArray) {
   });
 }
 
-// Vänd bild när man klickar på den - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Flip card when clicked and count clicks - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 let hasFlippedCard = false;
 let card1, card2;
 
@@ -38,7 +45,8 @@ for (let i = 0; i < cards.length; i++) {
 
 function flipCard() {
   this.classList.add("flipped");
-  // "flipped" togglas inte, bara aktiveras
+  clicks++;
+  currentClicks.innerHTML = " Clicks: " + clicks;
 
   if (!hasFlippedCard) {
     hasFlippedCard = true;
@@ -50,11 +58,12 @@ function flipCard() {
   checkForMatch();
 }
 
+// Check if cards match, if so: disable them, add points and run checkScore-function to se if you've won - DONE!!!!!!!!!!!!
 function checkForMatch() {
   if (card1.dataset.cardname === card2.dataset.cardname) {
     disableCards();
     points++;
-    console.log(points);
+    currentScore.innerHTML = " Points: " + points;
     checkScore();
     return;
   }
@@ -62,11 +71,13 @@ function checkForMatch() {
   flipBackCards();
 }
 
+// Disable cards that has been matched so you can't click on them again - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function disableCards() {
   card1.removeEventListener("click", flipCard);
   card2.removeEventListener("click", flipCard);
 }
 
+// Flip cards that did not match so they face down again - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function flipBackCards() {
   setTimeout(() => {
     card1.classList.remove("flipped");
@@ -74,37 +85,30 @@ function flipBackCards() {
   }, 1000);
 }
 
-// Klickcounter
-function countClicks() {
-  let clickableCards = document.querySelectorAll(".card");
-  let yourScore = document.querySelector(".points");
-  clickableCards.onclick = function() {
-    clicks++;
-    yourScore.innerHTML = "Your point: " + clicks;
-    console.log(clicks);
-  };
-}
-
-// När man fått 10 poäng = "You won!" - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Checks if you have 10 point and displays winner-message
 function checkScore() {
   if (points === 10) {
     console.log("You won!");
   }
 }
 
-// Reset function - Lägg till: nollställ poäng-countern och klick-countern, nollställ classer
+// Time-counter
+
+// Resets click-counter, point-counter and removes "flipped"-class - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function reset() {
   shuffleImages(images);
-  let point = 0;
-  let clicks = 0;
+  point = 0;
+  clicks = 0;
+  currentScore.innerHTML = " Points: " + points;
+  currentClicks.innerHTML = " Clicks: " + clicks;
   cards.forEach((card, i) => {
     card.classList.remove("flipped");
   });
 }
 
-//Reset game when browser reloads - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Reset game when browser reloads - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 window.onload = reset();
 
-//Reset game when play-again-button is clicked - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Reset game when play-again-button is clicked - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 const playAgainButton = document.querySelector("button");
 playAgainButton.addEventListener("click", reset);
