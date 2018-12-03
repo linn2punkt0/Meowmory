182f786024666dd3cd241bb4a266573c484d5977
Linn2punkt0 <linn.johansson7@hotmail.com>
2018-12-03 13:46:02 +0100

    Added timer and blocked clicking third card

--- main.js
+++ main.js
@@ -12,15 +12,20 @@ const images = [
 ];
 let points = 0;
 let clicks = 0;
+let seconds = 0;
+let startTime = null;
 
-// Show current score - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Show current score
 let currentScore = document.querySelector(".points");
 currentScore.innerHTML = " Points: " + points;
 
 let currentClicks = document.querySelector(".clicks");
 currentClicks.innerHTML = " Clicks: " + clicks;
 
-//Shuffle the images and place them in the card divs - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+let currentTime = document.querySelector(".time");
+currentTime.innerHTML = "Time: " + seconds + " seconds";
+
+//Shuffle the images and place them in the card divs
 const cards = document.querySelectorAll(".card");
 
 function shuffleImages(imageArray) {
@@ -34,50 +39,62 @@ function shuffleImages(imageArray) {
   });
 }
 
-// Flip card when clicked and count clicks - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Flip card when clicked, start timer and count clicks
 let hasFlippedCard = false;
 let card1, card2;
+let hasTwoCards = false;
 
 for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", flipCard);
 }
-// const flippedCards = document.querySelectorAll(".card.flipped"); Behövs inte längre?
+let interval = null;
 
 function flipCard() {
-  this.classList.add("flipped");
-  clicks++;
-  currentClicks.innerHTML = " Clicks: " + clicks;
+  if (!hasTwoCards) {
+    if (!startTime) {
+      startTime = Date.now();
+      interval = setInterval(timer, 1000);
+    }
+    this.classList.add("flipped");
+    clicks++;
+    currentClicks.innerHTML = " Clicks: " + clicks;
 
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    card1 = this;
-    return;
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      card1 = this;
+      return;
+    }
+    card2 = this;
+    hasFlippedCard = false;
+    hasTwoCards = true;
+    checkForMatch();
   }
-  card2 = this;
-  hasFlippedCard = false;
-  checkForMatch();
 }
 
-// Check if cards match, if so: disable them, add points and run checkScore-function to se if you've won - DONE!!!!!!!!!!!!
+// Check if cards match, if so: disable them, add points and run checkScore-function to se if you've won
+// If cards do not match: flip them back
 function checkForMatch() {
   if (card1.dataset.cardname === card2.dataset.cardname) {
     disableCards();
     points++;
     currentScore.innerHTML = " Points: " + points;
     checkScore();
+    hasTwoCards = false;
     return;
   }
-  // lås pointerEvents om man trycker på två kort
   flipBackCards();
+  setTimeout(() => {
+    hasTwoCards = false;
+  }, 1000);
 }
 
-// Disable cards that has been matched so you can't click on them again - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Disable cards that has been matched so you can't click on them again
 function disableCards() {
   card1.removeEventListener("click", flipCard);
   card2.removeEventListener("click", flipCard);
 }
 
-// Flip cards that did not match so they face down again - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Flip cards that did not match so they face down again
 function flipBackCards() {
   setTimeout(() => {
     card1.classList.remove("flipped");
@@ -88,27 +105,36 @@ function flipBackCards() {
 // Checks if you have 10 point and displays winner-message
 function checkScore() {
   if (points === 10) {
+    clearInterval(interval);
     console.log("You won!");
   }
 }
 
-// Time-counter
+//Timer-function
+function timer() {
+  const time = Date.now() - startTime;
+  currentTime.innerHTML = "Time: " + Math.round(time / 1000) + " seconds";
+}
 
-// Resets click-counter, point-counter and removes "flipped"-class - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Resets click-counter, point-counter and removes "flipped"-class
 function reset() {
   shuffleImages(images);
-  point = 0;
+  clearInterval(interval);
+  points = 0;
   clicks = 0;
+  startTime = null;
   currentScore.innerHTML = " Points: " + points;
   currentClicks.innerHTML = " Clicks: " + clicks;
+  currentTime.innerHTML = "Time: " + seconds + " seconds";
   cards.forEach((card, i) => {
     card.classList.remove("flipped");
+    cards[i].addEventListener("click", flipCard);
   });
 }
 
-// Reset game when browser reloads - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Reset game when browser reload
 window.onload = reset();
 
-// Reset game when play-again-button is clicked - DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Reset game when play-again-button is clicked
 const playAgainButton = document.querySelector("button");
 playAgainButton.addEventListener("click", reset);
